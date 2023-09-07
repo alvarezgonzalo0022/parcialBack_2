6649e0dba29b2bce991eba9d89458853416ddd9d
gonza <alvarezgonzalo0022@gmail.com>
2023-09-07 19:33:17 -0300

    Added logic to drcypt groups

--- Ejercicio-tp/ms-bills/src/main/java/com/msbills/security/KeyCloakJwtAuthenticationConverter.java
+++ Ejercicio-tp/ms-bills/src/main/java/com/msbills/security/KeyCloakJwtAuthenticationConverter.java
@@ -27,7 +27,6 @@ public class KeyCloakJwtAuthenticationConverter implements Converter<Jwt, Abstra
     objectMapper.registerModule(new JavaTimeModule());
     resourcesRoles.addAll(extractRoles("resource_access", objectMapper.readTree(objectMapper.writeValueAsString(jwt)).get("claims")));
     resourcesRoles.addAll(extractRolesRealmAccess("realm_access", objectMapper.readTree(objectMapper.writeValueAsString(jwt)).get("claims")));
-    resourcesRoles.addAll(extractAud("aud", objectMapper.readTree(objectMapper.writeValueAsString(jwt)).get("claims")));
     resourcesRoles.addAll(extractGroups("groups", objectMapper.readTree(objectMapper.writeValueAsString(jwt)).get("claims")));
     return resourcesRoles;
   }
@@ -60,19 +59,6 @@ public class KeyCloakJwtAuthenticationConverter implements Converter<Jwt, Abstra
 
     return authorityList;
   }
-  private static List<GrantedAuthority> extractAud(String route, JsonNode jwt) {
-    Set<String> rolesWithPrefix = new HashSet<>();
-
-    jwt.path(route)
-        .elements()
-        .forEachRemaining(e ->rolesWithPrefix.add("AUD_" + e.asText()));
-
-    final List<GrantedAuthority> authorityList =
-        AuthorityUtils.createAuthorityList(rolesWithPrefix.toArray(new String[0]));
-
-    return authorityList;
-  }
-
 
   private static List<GrantedAuthority> extractGroups(String route, JsonNode jwt) {
     Set<String> rolesWithPrefix = new HashSet<>();
@@ -86,7 +72,6 @@ public class KeyCloakJwtAuthenticationConverter implements Converter<Jwt, Abstra
 
     return authorityList;
   }
-
 
   public KeyCloakJwtAuthenticationConverter() {
   }
